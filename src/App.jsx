cfb54e5210c72e72b30fee2fe5144e2c7e22d200
conradjie <conrad.jie@gmail.com>
2023-09-15 16:24:29 +0200

    Opdracht 2 - Formulier

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,33 @@ function App() {
     const [appels, setAppels] = useState(0);
     const [kiwis, setKiwis] = useState(0);
 
+    const [formState, setFormState] = useState({
+        firstName: '',
+        lastName: '',
+        age: 0,
+        zipcode: '',
+        freq: '',
+        dayTime: '',
+        remark: '',
+        agreement: false,
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
+    function handleFormChange(e) {
+        const changedFieldName = e.target.name;
+        const newValue = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+        setFormState({
+            ...formState,
+            [changedFieldName]: newValue,
+
+        });
+    }
+
     return (
         <>
             <h1>Fruitmand bezorgservice</h1>
@@ -51,6 +78,67 @@ function App() {
                 }}
             >Reset
             </button>
+
+            <form className="order-form" onSubmit={handleSubmit}>
+                <label htmlFor="firstName">Voornaam</label>
+                <input type="text" id="firstName" name="firstName"
+                       value={formState.name}
+                       onChange={handleFormChange}
+                />
+                <label htmlFor="last-name">Achternaam</label>
+                <input type="text" id="lastName" name="lastName"
+                       value={formState.name}
+                       onChange={handleFormChange}
+                />
+                <label htmlFor="age">Leeftijd</label>
+                <input type="number" id="age" name="age"
+                       value={formState.name}
+                       onChange={handleFormChange}
+                />
+                <label htmlFor="zipcode">postcode</label>
+                <input type="text" id="zipcode" name="zipcode"
+                       value={formState.name}
+                       onChange={handleFormChange}
+                />
+                <label htmlFor="freq">Bezorgfrequentie</label>
+                <select id="freq" name="freq"
+                        value={formState.name}
+                        onChange={handleFormChange}
+                >
+                    <option value="weekly">Iedere week</option>
+                    <option value="everyotherweek">Om de week</option>
+                    <option value="monthly">Iedere maand</option>
+                </select>
+                <div className="dayTime">
+                    <label htmlFor="dayTime"><input type="radio" name="dayTime"
+                                                    value={formState.name}
+                                                    onChange={handleFormChange}
+                    />Overdag</label>
+                    <label htmlFor="dayTime"><input type="radio" name="dayTime"
+                                                    value={formState.name}
+                                                    onChange={handleFormChange}
+                    />s Avonds</label>
+                </div>
+                <div>
+                    <label htmlFor="remakr">Opmerking</label>
+                    <textarea className="remark" name="remark" id="remark"
+                              value={formState.name}
+                              onChange={handleFormChange}
+                    >
+                    </textarea>
+                </div>
+                <div className="agreement">
+                    <label htmlFor="agreement">
+                        <input type="checkbox" name="agreement" id="agreement"
+                               value={formState.name}
+                               onChange={handleFormChange}
+                        />Ik ga akkoord met de voorwaarden
+                    </label>
+                </div>
+                <button type="submit" disabled={!formState.agreement}>Verzend
+                </button>
+
+            </form>
         </>
     )
 }
